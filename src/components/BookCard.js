bca11fdae27715f62d3132e278db7ad4bc1e0e62
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-09-26 17:48:55 +0200

    Add id if missing. Extend object destructuring.

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,9 +4,9 @@ import Link from 'next/link'
 
 function BookCard({ bookInformation }) {
 
-    const { name, genre, coverUrl, description } = bookInformation;
-    console.log('books', bookInformation);
-  return (
+    const { name, genre, coverUrl, description, averageRating, haveRead, currentlyReading, wantToRead } = bookInformation;
+
+    return (
     <section>
         <Link href="#">
             <div className="max-w-sm rounded overflow-hidden w-full shadow-lg">
@@ -15,10 +15,11 @@ function BookCard({ bookInformation }) {
                     alt={name}
                     width={300}
                     height={200}
+                    priority
                     className="object-cover h-48 w-96"
                 />
             <div className="px-6 py-4 h-80">
-                <div className="font-bold text-xl mb-2">{name}</div>
+                <div className="font-bold text-xl mb-2 capitalize">{name}</div>
                 <div className="text-gray-700 text-base">{description}</div>
             </div>
             <div className="px-6 pt-4 pb-2">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import BookCard from "@/components/BookCard"
 import deviesFetch from "@/api/axios";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
 
@@ -23,15 +24,15 @@ export default function Home() {
       className="min-h-screen p-16"
     >
       <div className="text-4xl font-bold text-center">Devies Reads</div>
-      <div className="text-xl font-bold text-center">The ultimate programmers book list</div>
+      <div className="text-2xl font-bold text-center tracking-wider">Because dev's read</div>
       <div className="mt-10 flex flex-wrap gap-10">
-        {books.map((book) => {
-          return (
-            <div key={book.id}>
+        {books.map(book => (
+          book.id === undefined ?
+          (book.id = uuidv4()) :
+          (<div key={book.id}>
               <BookCard bookInformation={book} />
-            </div>
-          );
-        })}
+            </div>)
+          ))}
       </div>
     </main>
   )
