c81f8c99a7847bd95ca1873d32ea0160419a9c6f
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-09-27 00:06:16 +0200

    Additional data.

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -18,9 +18,15 @@ function BookCard({ bookInformation }) {
                     priority
                     className="object-cover h-48 w-96"
                 />
-            <div className="px-6 py-4 h-80">
+            <div className="px-6 py-4 h-[350px]">
                 <div className="font-bold text-xl mb-2 capitalize">{name}</div>
                 <div className="text-gray-700 text-base">{description}</div>
+            </div>
+            <div className="px-6 py-4 mt-6 flex justify-between">
+                <div>{wantToRead}</div>
+                <div>{averageRating.toFixed(1)}</div>
+                <div>{haveRead}</div>
+                <div>{currentlyReading}</div>
             </div>
             <div className="px-6 pt-4 pb-2">
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{`#${genre}`}</span>
