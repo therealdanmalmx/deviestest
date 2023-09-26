291163956cd6867ed97ee71f1bc9b3bd5de0c520
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-09-27 00:06:25 +0200

    Filter bar

---
+++ src/components/Landing.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import BookCard from "@/components/BookCard"
+import FilterBar from "./FilterBar";
+import deviesFetch from "@/api/axios";
+import { v4 as uuidv4 } from 'uuid';
+
+const Landing = () => {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect( () => {
+      const fetchBooks = async () => {
+        try {
+          const response = await deviesFetch.get("/books");
+          setBooks(response.data);
+
+        } catch (error) {
+          console.log(error)
+        }
+      };
+      fetchBooks();
+    }, []);
+
+    return (
+        <section>
+            <div className="text-4xl font-bold text-center">Devies Reads</div>
+            <div className="text-2xl font-bold text-center tracking-wider">Because dev's read</div>
+                <FilterBar />
+            <div className="mt-10 flex flex-wrap gap-10">
+            {books.map(book => (
+                book.id === undefined ?
+                (book.id = uuidv4()) :
+                (<div key={book.id}>
+                    <BookCard bookInformation={book} />
+                </div>)
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Landing;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,11 @@
-import { useState, useEffect } from "react";
-import BookCard from "@/components/BookCard"
-import deviesFetch from "@/api/axios";
-import { v4 as uuidv4 } from 'uuid';
+import Landing from "@/components/Landing";
 
 export default function Home() {
-
-  const [books, setBooks] = useState([]);
-
-  useEffect( () => {
-    const fetchBooks = async () => {
-      try {
-        const response = await deviesFetch.get("/books");
-        setBooks(response.data);
-
-      } catch (error) {
-        console.log(error)
-      }
-    };
-    fetchBooks();
-  }, []);
   return (
     <main
       className="min-h-screen p-16"
     >
-      <div className="text-4xl font-bold text-center">Devies Reads</div>
-      <div className="text-2xl font-bold text-center tracking-wider">Because dev's read</div>
-      <div className="mt-10 flex flex-wrap gap-10">
-        {books.map(book => (
-          book.id === undefined ?
-          (book.id = uuidv4()) :
-          (<div key={book.id}>
-              <BookCard bookInformation={book} />
-            </div>)
-          ))}
-      </div>
+      <Landing />
     </main>
   )
 }
