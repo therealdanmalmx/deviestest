7a3a01d988c450ebbb6c575e314f623587445a30
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-09-28 11:05:30 +0200

    Add filtering

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,23 +2,12 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookOpen, faBookmark, faStar, faBook } from '@fortawesome/free-solid-svg-icons';
 
-const FilterBar = ({ books }) => {
+const FilterBar = ({ onSort }) => {
 
-    const {averageRating, haveRead, currentlyReading, wantToRead} = books;
 
-    console.log('books from landing', books);
-
-    const rateBook = () => {
-        console.log('Book rated')
-    };
-    const wishToRead = () => {
-        console.log('Want to read')
-    };
-    const alreadyRead = () => {
-        console.log('Have read')
-    };
-    const reading = () => {
-        console.log('Reading')
+    const handleFilterChange = (e) => {
+        const value = e.target.value;
+        onFilter(value);
     };
 
   return (
@@ -27,16 +16,16 @@ const FilterBar = ({ books }) => {
         <div className="text-2xl font-bold text-center tracking-wider">Because dev's read</div>
         <div className="px-6 py-4 mt-6 flex justify-start align-middle bg-slate-200">
             <h1 className='font-bold'>Filter options:</h1>
-            <div className='ml-12' onClick={rateBook} style={{cursor: 'pointer'}}>
+            <div className='ml-12' onClick={() => onSort('highestRating')} style={{cursor: 'pointer'}}>
                 <FontAwesomeIcon icon={faStar} size="sm" /><span className='ml-1'>Average rating</span>
             </div>
-            <div className='ml-8' onClick={wishToRead} style={{cursor: 'pointer'}}>
+            <div className='ml-8' onClick={() => onSort('mostWanted')} style={{cursor: 'pointer'}}>
                 <FontAwesomeIcon icon={faBookmark} size="sm" /><span className='ml-1'>Want to Read</span>
             </div>
-            <div className='ml-8' onClick={alreadyRead} style={{cursor: 'pointer'}}>
+            <div className='ml-8' onClick={() => onSort('mostRead')} style={{cursor: 'pointer'}}>
                 <FontAwesomeIcon icon={faBook} size="sm" /><span className='ml-1'>Have Read</span>
             </div>
-            <div className='ml-8' onClick={reading} style={{cursor: 'pointer'}}>
+            <div className='ml-8' onClick={() => onSort('currentReading')} style={{cursor: 'pointer'}}>
                 <FontAwesomeIcon icon={faBookOpen} size="sm" /><span className='ml-1'>Currently Reading</span>
             </div>
         </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 const Landing = () => {
 
     const [books, setBooks] = useState([]);
+    const [filteredRanking, setFilteredRanking] = useState([]); // new state for filtered books
 
     useEffect( () => {
       const fetchBooks = async () => {
@@ -21,9 +22,50 @@ const Landing = () => {
       fetchBooks();
     }, []);
 
+    const sortByMostRead = () => {
+      const sortedBooks = [...books].sort((a, b) => b.haveRead - a.haveRead);
+      setBooks(sortedBooks);
+    };
+
+    const sortByMostWanted = () => {
+        const sortedBooks = [...books].sort((a, b) => b.wantToRead - a.wantToRead);
+        setBooks(sortedBooks);
+    };
+
+    const sortByCurrentlyReading = () => {
+        const sortedBooks = [...books].sort((a, b) => b.currentlyReading - a.currentlyReading);
+        setBooks(sortedBooks);
+    };
+    const sortByHighestRating = () => {
+        const sortedBooks = [...books].sort((a, b) => b.averageRating - a.averageRating);
+        setBooks(sortedBooks);
+    };
+
+    const handleSort = (sortType) => {
+        switch(sortType) {
+            case 'mostRead':
+                sortByMostRead();
+                break;
+            case 'mostWanted':
+                sortByMostWanted();
+                break;
+            case 'highestRating':
+                sortByHighestRating();
+                break;
+            case 'currentReading':
+                sortByCurrentlyReading();
+                break;
+            case 'name':
+                sortByName();
+                break;
+            default:
+                break;
+        }
+    };
+
     return (
         <section>
-            <FilterBar books={books} />
+            <FilterBar onSort={handleSort} />
             <div className="mt-10 flex flex-wrap gap-10">
             {books.map(book => (
                 book.id !== undefined &&
